f4b76b5430b7d17e1b48c234497bc170d4b905e7
Lokesh Singh Mehta <lokeshsinghmehta686@gmail.com>
2024-11-15 03:12:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import DisplayName from './Form'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <DisplayName/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Form.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const DisplayName = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (firstName.trim() && lastName.trim() ||) {
+      setFullName(`${firstName} ${lastName}`);
+    } else {
+      alert("please fill in both fields");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Full Name Display</h1>
+        <label htmlFor="firstName"> First Name:</label>
+        <input id="firstName" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        <br />
+        <label htmlFor="lastName"> Last Name:</label>
+        <input id="lastName" type="text" value={lastName} onChange={(e)=> setLastName(e.target.value)} />
+        <br />
+        <button>Submit</button>
+      </form>
+      <h3>Full Name:{fullName}</h3>
+    </div>
+  );
+};
+
+export default DisplayName;
